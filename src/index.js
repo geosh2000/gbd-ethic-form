6f716216d4d6f9dd9f548da4b45d90111b1026a5
Jorge Sanchez <jorge.sanchez@adh.com>
2024-11-06 09:43:09 -0500

    index.js modificado para development

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
-// src/index.js
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap
 import App from './App';
 
-// const container = document.getElementById('root')
-// const root = createRoot(container);
-// root.render(<App unidad='atpm' />)
+// Detección del entorno: si estás en modo de desarrollo, renderiza automáticamente
+if (process.env.NODE_ENV === 'development') {
+  const rootElement = document.getElementById('root');
+  if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(<App unidad="atpm" />);
+  }
+}
 
 // Función para inyectar la aplicación React en un elemento específico del DOM
 export function renderApp(elementId) {
   const rootElement = document.getElementById(elementId);
-  
-  // Usa ReactDOM.createRoot() en lugar de ReactDOM.render()
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<App />);
+  if (rootElement) {
+    const root = createRoot(rootElement);
+    root.render(<App />);
+  }
 }
 
-// Hacer que la función renderApp sea accesible globalmente como MyReactApp
+// Hacer que la función renderApp sea accesible globalmente como EthicForm
 window.EthicForm = {
   renderApp
 };
-
